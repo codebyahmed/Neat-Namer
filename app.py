5f3030c324c3b257dc035ae66682852260bb1784
codebyahmed <ahmediqbal.dev@gmail.com>
2024-09-29 18:48:47 +0500

    Add table page with functionality to Add and Clear files

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+TEMP_FILES_DIR = '/tmp'
+selected_files = []
 
 
 @app.route('/')
@@ -12,6 +16,40 @@ def index():
 def rename():
     files = request.files.getlist('files')
     pass
+
+
+@app.route('/add_files', methods=['POST'])
+def add_files():
+    files = request.files.getlist('files')
+    global selected_files
+    new_files = []
+
+    for file in files:
+        filename = file.filename
+        selected_files.append(filename)
+        new_files.append(filename)
+        file_path = os.path.join(TEMP_FILES_DIR, filename)
+        file.save(file_path)
+    
+    return f"Files added: {', '.join(new_files)}"
+
+
+@app.route('/get_selected_files', methods=['GET'])
+def get_selected_files():
+    global selected_files
+    return selected_files
+
+
+@app.route('/clear_selected_files', methods=['DELETE'])
+def clear_selected_files():
+    global selected_files
+
+    for file in selected_files:
+        file_path = os.path.join(TEMP_FILES_DIR, file)
+        os.remove(file_path)
+
+    selected_files = []
+    return 'Selected files cleared'
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
     const homePage = document.getElementById('homePage');
-    const fileInput = document.getElementById('fileInput');
+    const fileInputAtHome = document.getElementById('fileInputAtHome');
     const addFilesBtn = document.getElementById('addFilesBtn');
 
+    const tablePage = document.getElementById('tablePage');
+    const fileInputAtTable = document.getElementById('fileInputAtTable');
+    const addMoreFilesBtn = document.getElementById('addMoreFilesBtn');
+    const clearFilesBtn = document.getElementById('clearFilesBtn');
+    const renameFilesBtn = document.getElementById('renameFilesBtn');
+    const saveFilesBtn = document.getElementById('saveFilesBtn');
+    const filesTableBody = document.getElementById('filesTableBody');
+    
+
     addFilesBtn.addEventListener('click', () => {
-        fileInput.click();  // Trigger the file input click
+        fileInputAtHome.click();
     });
 
-    fileInput.addEventListener('change', async (event) => {
-        const selectedFiles = Array.from(event.target.files); // Get selected files
+    fileInputAtHome.addEventListener('change', async (event) => {
+        const selectedFiles = Array.from(event.target.files);
+
+        if (selectedFiles.length > 0) {
+            homePage.style.display = 'none';
+            tablePage.style.display = 'block';
+
+            const formData = new FormData();
+            selectedFiles.forEach(file => {
+                formData.append('files', file);
+
+                const row = filesTableBody.insertRow();
+                row.insertCell(0).textContent = filesTableBody.children.length;
+                row.insertCell(1).textContent = file.name;
+            });
+
+            const response = await fetch('/add_files', {
+                method: 'POST',
+                body: formData
+            });
+            if (!response.ok) {
+                console.error('Error adding files');
+                return;
+            }
+        }
+    });
+
+    addMoreFilesBtn.addEventListener('click', () => {
+        fileInputAtTable.click();
+    });
+
+    fileInputAtTable.addEventListener('change', async (event) => {
+        const selectedFiles = Array.from(event.target.files);
+
+        if (selectedFiles.length > 0) {
+            const formData = new FormData();
+            selectedFiles.forEach(file => {
+                formData.append('files', file);
+
+                const row = filesTableBody.insertRow();
+                row.insertCell(0).textContent = filesTableBody.children.length;
+                row.insertCell(1).textContent = file.name;
+            });
+
+            const response = await fetch('/add_files', {
+                method: 'POST',
+                body: formData
+            });
+            if (!response.ok) {
+                console.error('Error adding files');
+                return;
+            }
+        }
+    });
+
+    clearFilesBtn.addEventListener('click', async () => {
+        homePage.style.display = 'block';
+        tablePage.style.display = 'none';
+
+        const response = await fetch('/clear_selected_files', {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+            console.error('Error clearing files');
+            return;
+        }
+
+        while (filesTableBody.firstChild) {
+            filesTableBody.removeChild(filesTableBody.firstChild);
+        }
     });
 });
